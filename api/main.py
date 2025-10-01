795a60bb24c749fbdad8b11834b0e8af36eb40d0
Kevin Rodríguez <kyrodriguez97@gmail.com>
2025-09-30 22:27:46 -0500

    feat(api): add endpoints for TRM

--- api/main.py
+++ api/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
 from src.routes.v1 import inflation as inflation_router
+from src.routes.v1 import trm as trm_router
 
 app = FastAPI()
 
 # Add routes
 app.include_router(inflation_router.router, prefix="/v1", tags=["inflation"])
+app.include_router(trm_router.router, prefix="/v1", tags=["trm"])
 
 # Add pagination support
 add_pagination(app)
--- api/src/schemas/trm.py
+++ api/src/schemas/trm.py
@@ -1,13 +1,27 @@
 from datetime import date
-from uuid import UUID
 
-from pydantic import BaseModel
+from fastapi import Query
+from fastapi_pagination import Params
+from pydantic import BaseModel, Field
 
 
-class InflationData(BaseModel):
-    id: UUID
+class TRMData(BaseModel):
     date: date
     value: float
 
     class Config:
         from_attributes = True
+
+
+class TRMByDate(BaseModel):
+    specific_date: date = Query(description="Date to query for TRM data")
+
+
+class TRMByDateRange(BaseModel):
+    start_date: date = Field(..., description="Start date for the range")
+    end_date: date = Field(..., description="End date for the range")
+
+
+class TRMPaginateParams(Params):
+    page: int = Query(1, ge=1, description="Page number")
+    size: int = Query(25, ge=1, le=100, description="Page size")
