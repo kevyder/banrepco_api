829c869176c85490b47299b34ec153f2b056cf5f
Kevin Rodríguez <kyrodriguez97@gmail.com>
2025-12-30 18:09:13 -0500

    feat: add cron job for monthly inflation

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,20 +1,19 @@
 import { env } from "cloudflare:workers";
-import { Container, getRandom } from "@cloudflare/containers";
-import { fetchLatestTRM } from './services/trm';
-import { DbService } from './services/db';
+import { Container, getContainer } from "@cloudflare/containers";
+import { getDailyTRM, getMonthlyInflation } from "./scheduledTask";
+
 
 export interface Env {
   BANREPCO_API_CACHE: KVNamespace;
   API_CONTAINER: DurableObjectNamespace<Container>;
   BANREP_WEB_SERVICE_URL: string;
-  INSTANCE_COUNT?: string;
+  INFLATION_WEB_SERVICE_URL: string;
   CACHE_EXPIRATION_TTL?: string;
   ENVIRONMENT?: string;
   DATABASE_URL: string;
   DATABASE_AUTH_TOKEN: string;
 }
 
-const INSTANCE_COUNT = parseInt(env.INSTANCE_COUNT || "10");
 const CACHE_EXPIRATION_TTL = parseInt(env.CACHE_EXPIRATION_TTL || "60"); // Default to 1 hour
 
 export class APIContainer extends Container {
@@ -46,25 +45,15 @@ export default {
     env: Env,
     ctx: ExecutionContext,
   ) {
-    try {
-      const banrepWebServiceUrl = env.BANREP_WEB_SERVICE_URL
-      const trmValue = await fetchLatestTRM(banrepWebServiceUrl);
-      console.log("TRM Value:", trmValue);
-
-      if (env.DATABASE_URL && env.DATABASE_AUTH_TOKEN) {
-        const dbService = new DbService({
-          url: env.DATABASE_URL,
-          authToken: env.DATABASE_AUTH_TOKEN
-        });
-
-        await dbService.insertTRM(trmValue);
-        console.log("TRM value stored in database successfully");
-      } else {
-        console.error('Database configuration not found in environment variables');
-      }
-    } catch (error) {
-      console.error('Failed to fetch and store TRM:', error);
+    switch (controller.cron) {
+      case "0 5 * * *": // Every day at 5 AM
+        await getDailyTRM(env);
+        break;
+      case "0 5 15 * *": // On the 15th of every month at 5 AM
+        await getMonthlyInflation(env);
+        break;
     }
+
   },
 
 
@@ -75,7 +64,8 @@ export default {
       // Check cache first
       let cacheData = await env.BANREPCO_API_CACHE.get(cacheKey, { type: "json" });
       if (!cacheData) {
-        const containerInstance = await getRandom(env.API_CONTAINER, INSTANCE_COUNT);
+        const { "session-id": sessionId } = await request.json();
+        const containerInstance = getContainer(env.MY_CONTAINER, sessionId);
         const response = await containerInstance.fetch(request);
         const responseData = await response.clone().text();
         await env.BANREPCO_API_CACHE.put(cacheKey, responseData, { expirationTtl: CACHE_EXPIRATION_TTL });
