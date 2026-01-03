2e6ea427127f331579c305a72ed86cc25b0174e7
Kevin Rodríguez <kyrodriguez97@gmail.com>
2026-01-02 21:29:43 -0500

    feat(worker): remove api container

--- js/src/index.ts
+++ js/src/index.ts
@@ -4,38 +4,11 @@ import { getDailyTRM, getMonthlyInflation } from "./scheduledTask";
 
 
 export interface Env {
-  BANREPCO_API_CACHE: KVNamespace;
-  API_CONTAINER: DurableObjectNamespace<Container>;
   BANREP_WEB_SERVICE_URL: string;
   INFLATION_WEB_SERVICE_URL: string;
-  CACHE_EXPIRATION_TTL?: string;
   ENVIRONMENT?: string;
   DATABASE_URL: string;
   DATABASE_AUTH_TOKEN: string;
-}
-
-const CACHE_EXPIRATION_TTL = parseInt(env.CACHE_EXPIRATION_TTL || "60"); // Default to 1 hour
-
-export class APIContainer extends Container {
-  defaultPort = 3000;
-  sleepAfter = "5m";
-  envVars = {
-    ENVIRONMENT: env.ENVIRONMENT,
-    DATABASE_URL: env.DATABASE_URL,
-    DATABASE_AUTH_TOKEN: env.DATABASE_AUTH_TOKEN,
-  };
-
-  onError(error: unknown) {
-    console.error(error);
-  }
-
-  onStart() {
-    console.log("Container started");
-  }
-
-  onStop() {
-    console.log("Container stopped");
-  }
 }
 
 export default {
@@ -56,26 +29,7 @@ export default {
 
   },
 
-
   async fetch(request: Request, env: Env) {
-    const url = new URL(request.url);
-    if (url.pathname.startsWith("/v1")) {
-      const cacheKey = url.toString();
-      // Check cache first
-      let cacheData = await env.BANREPCO_API_CACHE.get(cacheKey, { type: "json" });
-      if (!cacheData) {
-        const containerInstance = getContainer(env.API_CONTAINER);
-        const response = await containerInstance.fetch(request);
-        const responseData = await response.clone().text();
-        await env.BANREPCO_API_CACHE.put(cacheKey, responseData, { expirationTtl: CACHE_EXPIRATION_TTL });
-        return response;
-      } else {
-        return new Response(JSON.stringify(cacheData), {
-          headers: { "Content-Type": "application/json" },
-        });
-      }
-    }
-
     return new Response("Not found", { status: 404 });
   },
 };
